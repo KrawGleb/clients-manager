d67e213c6928e52c7d9b224617774cab12534a52
KrawGleb <krawcevitsch@gmail.com>
2022-08-27 15:43:48 +0300

    Remove MainWindow, add ShellView, add DI container

---
+++ ClientsManager.App/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using ClientsManager.App.ViewModels;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClientsManager.App;
+
+public class Bootstrapper : BootstrapperBase
+{
+	private readonly SimpleContainer _container = new();
+
+	public Bootstrapper()
+	{
+		Initialize();
+	}
+
+	protected override void Configure()
+	{
+
+	}
+
+	protected override void OnStartup(object sender, StartupEventArgs e)
+	{
+		DisplayRootViewForAsync<ShellViewModel>();
+	}
+}
