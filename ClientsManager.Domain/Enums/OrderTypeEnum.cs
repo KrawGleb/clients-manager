adc75f9c6ffe27573ef9adc8403b246b49763cee
KrawGleb <krawcevitsch@gmail.com>
2022-08-27 20:20:18 +0300

    Add order info model

---
+++ ClientsManager.Domain/Enums/OrderTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientsManager.Domain.Enums;
+
+
+public enum OrderType
+{
+    CarWash = 0,
+    CarService = 1
+}
---
+++ ClientsManager.Domain/Models/OrderInfo.cs
@@ -0,0 +1,38 @@
+﻿using ClientsManager.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientsManager.Domain.Models
+{
+    public class OrderInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [MaxLength(100)]
+        public string? AdditionalName { get; set; }
+
+        [Phone]
+        [Required]
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; }
+
+        [MaxLength(30)]
+        public string CarModel { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required]
+        public OrderType OrderType { get; set; }
+    }
+}
