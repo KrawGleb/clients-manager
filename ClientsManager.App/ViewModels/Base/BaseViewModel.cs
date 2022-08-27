89d4f58799b11a276d5aa7039358e9d3520a2083
KrawGleb <krawcevitsch@gmail.com>
2022-08-28 00:18:24 +0300

    Add BaseViewModel

---
+++ ClientsManager.App/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ClientsManager.App.ViewModels.Base;
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (Equals(field, value))
+        {
+            return false;
+        }
+
+        OnPropertyChanged(propertyName);
+
+        return true;
+    }
+}
